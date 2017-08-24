783734044b2dbdd497bbbd6287c2afc38483f277
Sarah Hanson <sarahh.hanson@gmail.com>
2017-08-24 14:20:07 -0500

    Added basic insert function to bst

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const Bst = require('./src/tree');
 const Node = require('./src/node');
+
+
+const eq = [1, "+", 1, "-", 4];
+
+let tree = new Bst();
+
+for(let i = 0; i < eq.length; i++){
+  tree.insert(eq[i]);
+}
+
+console.log(tree);
--- src/node.js
+++ src/node.js
@@ -1,8 +1,8 @@
 'use strict';
 
 class Node {
-  constructor(value, left, right) {
-    this.data = value;
+  constructor(value, left=null, right=null) {
+    this.value = value;
     this.left = left;
     this.right = right;
   }
