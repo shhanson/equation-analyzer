fe2058159095ed4e4a60f8965d63219b3687fe44
Sarah Hanson <sarahh.hanson@gmail.com>
2017-08-25 10:31:09 -0500

    Made it work for command line, mostly

--- public/src/app.js
+++ public/src/app.js
@@ -1,46 +1,60 @@
-const Bst = require('./tree');
-const Node = require('./node');
-const COUNT = 10; //Arbitrary number of spaces for printing tree
 
-const str = document.getElementById('equation').value;
-const array = str.trim().split(' ');
+  const Bst = require('./tree');
+  const Node = require('./node');
+  const COUNT = 10; //Arbitrary number of spaces for printing tree
 
-const tree = new Bst();
 
-for (let i = 0; i < array.length; i++) {
-  tree.insert(array[i]);
-}
 
-console.log(tree);
-console.log(calculate(tree.root));
-printTree(tree.root);
 
-function calculate(node) {
-  if (!node.right && !node.left) {
-    return node.value;
-  }
-  return eval(`${calculate(node.left)} ${node.value} ${calculate(node.right)}`);
-}
+  let args = process.argv.slice(2);
+  console.log(process.argv);
 
-function printTree(node){
-  printTreeUtil(node, 0);
-}
+  const input = args.filter((item) => {
 
-function printTreeUtil(node, space){
-  if(!node){
-    return;
+  });
+
+  args = "2 + .55";
+
+  const array = args.trim().split(' ');
+
+  const tree = new Bst();
+
+  for (let i = 0; i < array.length; i++) {
+    tree.insert(array[i]);
   }
 
-  space += COUNT;
 
-  printTreeUtil(node.right, space);
-  console.log();
+  console.log("RESULT: " + calculate(tree.root));
+  printTree(tree.root);
 
-  for(let i = COUNT; i < space; i++){
-    process.stdout.write(" ");
+
+  function calculate(node) {
+
+    if (!node.right && !node.left) {
+      return node.value;
+    }
+    return eval(`${calculate(node.left)} ${node.value} ${calculate(node.right)}`);
   }
-  console.log(node.value);
 
-  printTreeUtil(node.left, space);
+  function printTree(node){
+    printTreeUtil(node, 0);
+  }
 
-}
+  function printTreeUtil(node, space){
+    if(!node){
+      return;
+    }
+
+    space += COUNT;
+
+    printTreeUtil(node.right, space);
+    console.log();
+
+    for(let i = COUNT; i < space; i++){
+      process.stdout.write(" ");
+    }
+    console.log(node.value);
+
+    printTreeUtil(node.left, space);
+
+  }
