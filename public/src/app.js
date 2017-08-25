50001cd0f97e06fd5e37f86dfc3d6bf79bfbaedc
Daniel Isham <danielisham1@gmail.com>
2017-08-25 09:49:35 -0500

    form added

--- public/src/app.js
+++ public/src/app.js
@@ -1,23 +1,21 @@
 const Bst = require('./src/tree');
 const Node = require('./src/node');
 
-const str = " 2 * 1 + 5 * 8 - 2";
+const str = document.getElementById('equation').value;
 const array = str.trim().split(' ');
 
-let tree = new Bst();
+const tree = new Bst();
 
-for(let i = 0; i < array.length; i++){
+for (let i = 0; i < array.length; i++) {
   tree.insert(array[i]);
 }
 
 console.log(tree);
 console.log(calculate(tree.root));
 
-function calculate(node){
-
-  if(!node.right && !node.left){
+function calculate(node) {
+  if (!node.right && !node.left) {
     return node.value;
   }
   return eval(`${calculate(node.left)} ${node.value} ${calculate(node.right)}`);
-
 }
