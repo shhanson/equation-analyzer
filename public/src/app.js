0c60d902edd4541e97645500481c86569cb9a0ef
Sarah Hanson <sarahh.hanson@gmail.com>
2017-08-27 12:13:17 -0500

    Added comments

--- public/src/app.js
+++ public/src/app.js
@@ -2,20 +2,24 @@
   const Node = require('./node');
   const COUNT = 10; //Arbitrary number of spaces for printing tree
 
+  // Capture the command line args, exclude "node" and "app.js"
   let args = process.argv.slice(2);
 
+  // Initialize a new tree
   const tree = new Bst();
 
+  // Insert each operand and operator from args into the tree
   for (let i = 0; i < args.length; i++) {
     tree.insert(args[i]);
   }
 
-  console.log(`RESULT: ${calculate(tree.root)}`)
+  // Print expression result and a visualization of the tree to the console
+  console.log(`RESULT (NOW WITH LESS EVAL()): ${calculate(tree.root)}`)
   printTree(tree.root);
 
 
+  // Recursive function to calculate the result of the expression
   function calculate(node) {
-
     if (!node.right && !node.left) {
       return node.value;
     }
@@ -23,6 +27,7 @@
     return evaluate(calculate(node.left), calculate(node.right), node.value);
   }
 
+  // Helper function to evaluate expressions
   function evaluate(op1, op2, operator){
     op1 = Number(op1);
     op2 = Number(op2);
@@ -46,10 +51,12 @@
     return result;
   }
 
+  // Helper function for printTreeUtil
   function printTree(node){
     printTreeUtil(node, 0);
   }
 
+  // Recursive function to print a tree representation to the console
   function printTreeUtil(node, space){
     if(!node){
       return;
--- public/src/tree.js
+++ public/src/tree.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Node = require('./node');
-const OPS = ['-', '+', 'x', '/'];
+const OPS = ['-', '+', 'x', '/']; // Changed '*' to 'x' for command line input
 
 class Bst {
   constructor(){
@@ -9,8 +9,10 @@ class Bst {
   }
 
   insert(value){
+    // Create a new node with the given value
     let newNode = new Node(value);
 
+    // If the tree is empty, the new node becomes the root
     if(!this.root){
       this.root = newNode;
       return this;
@@ -18,19 +20,28 @@ class Bst {
 
     // If the value is an operator
     if(OPS.includes(newNode.value)){
+      // If the operator is a "+" or "-", everything currently in the tree becomes the
+      // left operand
       if(newNode.value === "+" || newNode.value === "-"){
         newNode.left = this.root;
         this.root = newNode;
       } else {
+
         if(this.root.right){
+          // If the operator is a "x" or "/", it replaces the right child
+          // of root, and the former right child becomes the left child
+          // of the new operator.
           newNode.left = this.root.right;
           this.root.right = newNode;
-        } else {
-          newNode.left = this.root;
-          this.root = newNode;
-        }
+         }
+         else {
+           newNode.left = this.root;
+           this.root = newNode;
+         }
       }
     } else {
+      // If the new value is not an operator, descend to the
+      // right-most node of the tree
       let current = this.root;
       while(current.right !== null){
         current = current.right;
